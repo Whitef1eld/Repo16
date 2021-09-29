c889f4e1a365dc2338aff2f947b1fe9ac439be37
Whitef1eld <nwhitefiel1@etf.unsa.ba>
2021-09-29 09:47:29 +0200

    Added all the exercises for day16.

---
+++ ex03/src/index.js
@@ -0,0 +1,14 @@
+// only change code below this line
+class Person {
+    static display() {
+        var message = "Static method is invoked from Person class";
+        return message;
+    }
+    show(display) {
+        console.log(Person.display());
+    }
+}
+// only change code above this line
+var message = new Person();
+message.show();
+module.exports = Person;
